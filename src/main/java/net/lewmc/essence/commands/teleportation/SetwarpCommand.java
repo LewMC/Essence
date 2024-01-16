94c771b15baaf98641c0d21d8367c1412842df68
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-16 20:37:09 +0000

    Move to desktop.

--- src/main/java/net/lewmc/essence/commands/teleportation/SetwarpCommand.java
+++ src/main/java/net/lewmc/essence/commands/teleportation/SetwarpCommand.java
@@ -3,10 +3,15 @@ package net.lewmc.essence.commands.teleportation;
 import net.lewmc.essence.MessageHandler;
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.events.PermissionHandler;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
+
+import java.io.IOException;
 
 public class SetwarpCommand implements CommandExecutor {
     private Essence plugin;
@@ -30,16 +35,39 @@ public class SetwarpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (!(commandSender instanceof Player)) {
-            plugin.getLogger().warning("[Essence] Sorry, you need to be an in-game player to use this command.");
+            this.plugin.getLogger().warning("[Essence] Sorry, you need to be an in-game player to use this command.");
             return true;
         }
-        MessageHandler message = new MessageHandler(commandSender, plugin);
+        MessageHandler message = new MessageHandler(commandSender, this.plugin);
         Player player = (Player) commandSender;
         PermissionHandler permission = new PermissionHandler(player, message);
 
         if (command.getName().equalsIgnoreCase("setwarp")) {
             if (permission.has("essence.warp.create")) {
                 message.PrivateMessage("This command is temporarily unavailable due to technical issues.", true);
+                if (args.length == 0) {
+                    message.PrivateMessage("Usage: /setwarp <name>", true);
+                    return true;
+                }
+                Location loc = player.getLocation();
+                try {
+                    this.plugin.getConfig().load("warp.yml");
+                } catch (IOException e) {
+                    this.plugin.getLogger().warning("[Essence] RuntimeException: "+e);
+                } catch (InvalidConfigurationException e) {
+                    this.plugin.getLogger().warning("[Essence] InvalidConfigurationException: "+e);
+                }
+                this.plugin.getConfig().createSection(args[0].toLowerCase());
+
+                ConfigurationSection cs = this.plugin.getConfig().getConfigurationSection(args[0].toLowerCase());
+                cs.set("X", loc.getX());
+                cs.set("Y", loc.getY());
+                cs.set("Z", loc.getZ());
+                cs.set("world", loc.getWorld().getName());
+
+                this.plugin.saveConfig();
+
+                message.PrivateMessage("Created warp: " + args[0] + " at your location", false);
             } else {
                 permission.not();
             }
