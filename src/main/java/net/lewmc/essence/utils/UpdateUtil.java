8e1831399e316fb2fa000a5a545827236c88bbaa
Lewis Milburn <lewiscmilburn@gmail.com>
2024-01-19 11:23:56 +0000

    Added snapshot update checker.

--- src/main/java/net/lewmc/essence/utils/UpdateUtil.java
+++ src/main/java/net/lewmc/essence/utils/UpdateUtil.java
@@ -21,21 +21,31 @@ public class UpdateUtil {
     public void VersionCheck() {
         if (this.plugin.getConfig().getBoolean("update-check")) {
             try {
-                URL url = new URL("https://service.lewmc.net/latest-version/?resource=essence&format=simpleversion");
+                URL url;
+                if (this.plugin.getDescription().getVersion().contains("SNAPSHOT")) {
+                    log.warn("You are running a snapshot build of Essence.");
+                    log.warn("This build may include bugs and is not recommended on production servers.");
+                    log.warn("If you find any issues please report them to github.com/lewmilburn/essence.");
+                    url = new URL("https://service.lewmc.net/latest-version/?resource=essence-snapshot&format=simpleversion");
+                } else {
+                    url = new URL("https://service.lewmc.net/latest-version/?resource=essence&format=simpleversion");
+                }
                 Scanner s = new Scanner(url.openStream());
                 if (s.hasNextLine()) {
                     String response = s.nextLine();
                     if (response.equals("")) {
-                        log.warn("Unable to perform update check: There was no response from the server.");
+                        log.severe("Unable to perform update check: There was no response from the server.");
                     } else if (response.equals(this.plugin.getDescription().getVersion())) {
                         log.info("You are running the latest version of Essence.");
                     } else {
-                        log.warn("There's a new version of Essence available.");
-                        log.warn("Your version: "+this.plugin.getDescription().getVersion()+" - latest version: "+response);
+                        log.severe("There's a new version of Essence available.");
+                        log.severe("Your version: "+this.plugin.getDescription().getVersion()+" - latest version: "+response);
                     }
+                } else {
+                    log.severe("Unable to perform update check: There was no response from the server.");
                 }
             } catch (MalformedURLException e) {
-                log.warn("Unable to perform update check: MalformedURLException - "+e);
+                log.severe("Unable to perform update check: MalformedURLException - "+e);
             } catch (IOException e) {
                 log.warn("Unable to perform update check: IOException - "+e);
             }
