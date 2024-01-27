27dc38fa3380170806fdb2c931a6a7e206c04aad
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-27 16:26:02 +0000

    Removed unused imports

--- src/main/java/net/lewmc/essence/commands/TeamCommands.java
+++ src/main/java/net/lewmc/essence/commands/TeamCommands.java
@@ -3,8 +3,6 @@ package net.lewmc.essence.commands;
 import net.lewmc.essence.utils.MessageUtil;
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.utils.PermissionHandler;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
