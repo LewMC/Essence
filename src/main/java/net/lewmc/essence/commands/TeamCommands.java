2703423ac5a024dce3017e59fe017cbbf1b6db1a
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-27 16:45:49 +0000

    Optimise

--- src/main/java/net/lewmc/essence/commands/TeamCommands.java
+++ src/main/java/net/lewmc/essence/commands/TeamCommands.java
@@ -1,12 +1,9 @@
 package net.lewmc.essence.commands;
 
-import net.lewmc.essence.utils.MessageUtil;
 import net.lewmc.essence.Essence;
-import net.lewmc.essence.utils.PermissionHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class TeamCommands implements CommandExecutor {
