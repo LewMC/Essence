20206c74c8b5253b4114ef7a484e0083bd20b458
Lewis Milburn <lewiscmilburn@gmail.com>
2024-01-30 10:20:05 +0000

    Fix another tab complete issue.

--- src/main/java/net/lewmc/essence/tabcompleter/EssenceTabCompleter.java
+++ src/main/java/net/lewmc/essence/tabcompleter/EssenceTabCompleter.java
@@ -8,7 +8,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class EssenceTabCompleter implements TabCompleter {
     @Override
@@ -18,18 +17,20 @@ public class EssenceTabCompleter implements TabCompleter {
             @NotNull String arg,
             String[] args
     ) {
-        String[] keys = null;
-        if (args.length == 0) {
+        String[] keys;
+        if (args.length == 1) {
             keys = new String[]{"help"};
-        } else if (args.length == 1) {
-            keys = new String[]{"inventory", "gamemode", "teleport", "stats", "economy"};
+        } else if (args.length == 2) {
+            keys = new String[]{"inventory", "gamemode", "teleport", "stats", "economy", "team"};
         } else if (
-            args.length == 2 && (
+                args.length == 3 && (
                 args[1].equalsIgnoreCase("teleport") ||
                 args[1].equalsIgnoreCase("inventory")
             )
         ) {
             keys = new String[]{"1", "2"};
+        } else {
+            keys = new String[]{};
         }
 
         return new ArrayList<>(Arrays.asList(keys));
