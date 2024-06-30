d5468b2941573d97db2e69185d7f5f12ae598339
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-06-30 15:32:16 +0100

    Add commands and permissions

---
+++ src/main/java/net/lewmc/essence/commands/playerinfo/SeenCommand.java
@@ -0,0 +1,58 @@
+package net.lewmc.essence.commands.playerinfo;
+
+import net.lewmc.essence.Essence;
+import net.lewmc.essence.utils.LogUtil;
+import net.lewmc.essence.utils.MessageUtil;
+import net.lewmc.essence.utils.PermissionHandler;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * /seen command.
+ */
+public class SeenCommand implements CommandExecutor {
+    private final Essence plugin;
+    private final LogUtil log;
+
+    /**
+     * Constructor for the SeenCommand class.
+     * @param plugin References to the main plugin class.
+     */
+    public SeenCommand(Essence plugin) {
+        this.plugin = plugin;
+        this.log = new LogUtil(plugin);
+    }
+
+    /**
+     * @param commandSender Information about who sent the command - player or console.
+     * @param command Information about what command was sent.
+     * @param s Command label - not used here.
+     * @param args The command's arguments.
+     * @return boolean true/false - was the command accepted and processed or not?
+     */
+    @Override
+    public boolean onCommand(
+            @NotNull CommandSender commandSender,
+            @NotNull Command command,
+            @NotNull String s,
+            String[] args
+    ) {
+        MessageUtil message = new MessageUtil(commandSender, plugin);
+        Player player = (Player) commandSender;
+        PermissionHandler permission = new PermissionHandler(commandSender, message);
+
+        if (command.getName().equalsIgnoreCase("seen")) {
+            if (permission.has("essence.playerinfo.seen")) {
+                // TODO: IMPLEMENT
+                return true;
+            } else {
+                return permission.not();
+            }
+        }
+
+        return false;
+    }
+}
