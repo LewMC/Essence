0c89b691a5abc8ea1f76a546a1aaab2e2b6cdbae
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-19 23:06:04 +0100

    Migrate to Foundry

--- src/main/java/net/lewmc/essence/commands/admin/SeenCommand.java
+++ src/main/java/net/lewmc/essence/commands/admin/SeenCommand.java
@@ -3,21 +3,21 @@ package net.lewmc.essence.commands.admin;
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.utils.*;
 import net.lewmc.foundry.Files;
+import net.lewmc.foundry.command.FoundryCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * /seen command.
  */
-public class SeenCommand implements CommandExecutor {
+public class SeenCommand extends FoundryCommand {
     private final Essence plugin;
 
     /**
      * Constructor for the SeenCommand class.
+     *
      * @param plugin References to the main plugin class.
      */
     public SeenCommand(Essence plugin) {
@@ -25,54 +25,48 @@ public class SeenCommand implements CommandExecutor {
     }
 
     /**
-     * @param cs Information about who sent the command - player or console.
+     * The permission required to run the command.
+     * @return String - The permission string.
+     */
+    @Override
+    protected String requiredPermission() {
+        return "essence.playerinfo.seen";
+    }
+
+    /**
+     * @param cs      Information about who sent the command - player or console.
      * @param command Information about what command was sent.
-     * @param s Command label - not used here.
-     * @param args The command's arguments.
+     * @param s       Command label - not used here.
+     * @param args    The command's arguments.
      * @return boolean true/false - was the command accepted and processed or not?
      */
     @Override
-    public boolean onCommand(
-            @NotNull CommandSender cs,
-            @NotNull Command command,
-            @NotNull String s,
-            String[] args
-    ) {
-        if (command.getName().equalsIgnoreCase("seen")) {
-            CommandUtil cmd = new CommandUtil(this.plugin, cs);
-            if (cmd.isDisabled("seen")) { return cmd.disabled(); }
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        CommandUtil cmd = new CommandUtil(this.plugin, cs);
+        if (cmd.isDisabled("seen")) { return cmd.disabled(); }
 
-            PermissionHandler permission = new PermissionHandler(this.plugin, cs);
-
-            if (permission.has("essence.playerinfo.seen")) {
-                MessageUtil msg = new MessageUtil(this.plugin, cs);
-                if (args.length == 1) {
-                    OfflinePlayer p = Bukkit.getOfflinePlayer(args[0]);
-                    if (p.hasPlayedBefore()) {
-                        Files fu = new Files(this.plugin.config, this.plugin);
-                        if (fu.exists(fu.playerDataFile(p.getUniqueId()))) {
-                            fu.load(fu.playerDataFile(p.getUniqueId()));
-                            if (fu.getString("user.last-seen") != null) {
-                                msg.send("seen", "lastseen", new String[]{p.getName(), fu.getString("user.last-seen")});
-                            } else {
-                                msg.send("seen", "neverseen", new String[]{p.getName()});
-                            }
-                            fu.close();
-                        } else {
-                            msg.send("generic","playernotfound");
-                        }
+        MessageUtil msg = new MessageUtil(this.plugin, cs);
+        if (args.length == 1) {
+            OfflinePlayer p = Bukkit.getOfflinePlayer(args[0]);
+            if (p.hasPlayedBefore()) {
+                Files fu = new Files(this.plugin.config, this.plugin);
+                if (fu.exists(fu.playerDataFile(p.getUniqueId()))) {
+                    fu.load(fu.playerDataFile(p.getUniqueId()));
+                    if (fu.getString("user.last-seen") != null) {
+                        msg.send("seen", "lastseen", new String[]{p.getName(), fu.getString("user.last-seen")});
                     } else {
-                        msg.send("generic","playernotfound");
+                        msg.send("seen", "neverseen", new String[]{p.getName()});
                     }
+                    fu.close();
                 } else {
-                    msg.send("generic","playernotfound");
+                    msg.send("generic", "playernotfound");
                 }
-                return true;
             } else {
-                return permission.not();
+                msg.send("generic", "playernotfound");
             }
+        } else {
+            msg.send("generic", "playernotfound");
         }
-
-        return false;
+        return true;
     }
 }
