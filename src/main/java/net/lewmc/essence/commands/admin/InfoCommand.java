71b8209307f267c106d1325f42cde07d35905c8c
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-19 23:00:21 +0100

    Update

--- src/main/java/net/lewmc/essence/commands/admin/InfoCommand.java
+++ src/main/java/net/lewmc/essence/commands/admin/InfoCommand.java
@@ -3,19 +3,17 @@ package net.lewmc.essence.commands.admin;
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.utils.CommandUtil;
 import net.lewmc.essence.utils.MessageUtil;
-import net.lewmc.essence.utils.PermissionHandler;
 import net.lewmc.foundry.Files;
+import net.lewmc.foundry.command.FoundryCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * /info command.
  */
-public class InfoCommand implements CommandExecutor {
+public class InfoCommand extends FoundryCommand {
     private final Essence plugin;
 
     /**
@@ -27,6 +25,15 @@ public class InfoCommand implements CommandExecutor {
     }
 
     /**
+     * The permission required to run the command.
+     * @return String - The permission string.
+     */
+    @Override
+    protected String requiredPermission() {
+        return "essence.playerinfo.info";
+    }
+
+    /**
      * @param cs Information about who sent the command - player or console.
      * @param command Information about what command was sent.
      * @param s Command label - not used here.
@@ -34,50 +41,34 @@ public class InfoCommand implements CommandExecutor {
      * @return boolean true/false - was the command accepted and processed or not?
      */
     @Override
-    public boolean onCommand(
-            @NotNull CommandSender cs,
-            @NotNull Command command,
-            @NotNull String s,
-            String[] args
-    ) {
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        CommandUtil cmd = new CommandUtil(this.plugin, cs);
+        if (cmd.isDisabled("info")) { return cmd.disabled(); }
 
-        if (command.getName().equalsIgnoreCase("info")) {
-            CommandUtil cmd = new CommandUtil(this.plugin, cs);
-            if (cmd.isDisabled("info")) { return cmd.disabled(); }
-
-            PermissionHandler perms = new PermissionHandler(this.plugin, cs);
-
-            if (perms.has("essence.playerinfo.info")) {
-                MessageUtil message = new MessageUtil(this.plugin, cs);
-                if (args.length == 1) {
-                    OfflinePlayer p = Bukkit.getOfflinePlayer(args[0]);
-                    if (p.hasPlayedBefore()) {
-                        Files fu = new Files(this.plugin.config, this.plugin);
-                        if (fu.exists(fu.playerDataFile(p.getUniqueId()))) {
-                            fu.load(fu.playerDataFile(p.getUniqueId()));
-                            message.send("info", "uuid", new String[] { String.valueOf(p.getUniqueId()) });
-                            message.send("info", "lastname", new String[] { fu.getString("user.last-known-name") });
-                            if (fu.getString("user.last-seen") != null) {
-                                message.send("info", "lastseen", new String[]{fu.getString("user.last-seen")});
-                            } else {
-                                message.send("info", "neverseen");
-                            }
-                            fu.close();
-                        } else {
-                            message.send("generic","playernotfound");
-                        }
+        MessageUtil message = new MessageUtil(this.plugin, cs);
+        if (args.length == 1) {
+            OfflinePlayer p = Bukkit.getOfflinePlayer(args[0]);
+            if (p.hasPlayedBefore()) {
+                Files fu = new Files(this.plugin.config, this.plugin);
+                if (fu.exists(fu.playerDataFile(p.getUniqueId()))) {
+                    fu.load(fu.playerDataFile(p.getUniqueId()));
+                    message.send("info", "uuid", new String[]{String.valueOf(p.getUniqueId())});
+                    message.send("info", "lastname", new String[]{fu.getString("user.last-known-name")});
+                    if (fu.getString("user.last-seen") != null) {
+                        message.send("info", "lastseen", new String[]{fu.getString("user.last-seen")});
                     } else {
-                        message.send("generic","playernotfound");
+                        message.send("info", "neverseen");
                     }
+                    fu.close();
                 } else {
-                    message.send("generic","playernotfound");
+                    message.send("generic", "playernotfound");
                 }
-                return true;
             } else {
-                return perms.not();
+                message.send("generic", "playernotfound");
             }
+        } else {
+            message.send("generic", "playernotfound");
         }
-
-        return false;
+        return true;
     }
 }
--- src/main/java/net/lewmc/essence/commands/admin/InvisibleCommand.java
+++ src/main/java/net/lewmc/essence/commands/admin/InvisibleCommand.java
@@ -2,17 +2,15 @@ package net.lewmc.essence.commands.admin;
 
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.utils.*;
-import net.lewmc.foundry.Logger;
+import net.lewmc.foundry.command.FoundryConsoleCommand;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * /visible command.
  */
-public class InvisibleCommand implements CommandExecutor {
+public class InvisibleCommand extends FoundryConsoleCommand {
     private final Essence plugin;
 
     /**
@@ -25,6 +23,15 @@ public class InvisibleCommand implements CommandExecutor {
     }
 
     /**
+     * The permission required to run the command.
+     * @return String - The permission string.
+     */
+    @Override
+    protected String requiredPermission() {
+        return "essence.admin.invisible";
+    }
+
+    /**
      * The Invisible command.
      * @param cs Information about who sent the command - player or console.
      * @param command Information about what command was sent.
@@ -33,25 +40,10 @@ public class InvisibleCommand implements CommandExecutor {
      * @return boolean true/false - was the command accepted and processed or not?
      */
     @Override
-    public boolean onCommand(
-            @NotNull CommandSender cs,
-            @NotNull Command command,
-            @NotNull String s,
-            String[] args
-    ) {
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        CommandUtil cmd = new CommandUtil(this.plugin, cs);
+        if (cmd.isDisabled("invisible")) { return cmd.disabled(); }
 
-        if (command.getName().equalsIgnoreCase("invisible")) {
-            CommandUtil cmd = new CommandUtil(this.plugin, cs);
-            if (cmd.isDisabled("invisible")) { return cmd.disabled(); }
-
-            if (!(cs instanceof Player)) { return new Logger(this.plugin.config).noConsole(); }
-
-            PermissionHandler perm = new PermissionHandler(this.plugin, cs);
-            if (!perm.has("essence.admin.invisible")) { return perm.not(); }
-
-            return new StatsUtil(this.plugin, (Player) cs).toggleInvisible();
-        }
-
-        return false;
+        return new StatsUtil(this.plugin, (Player) cs).toggleInvisible();
     }
 }
