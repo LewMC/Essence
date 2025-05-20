9a644eab790a99e1dcdf7ca41abc30c91f837da3
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-20 09:00:29 +0100

    Migrate to Foundry.

--- src/main/java/net/lewmc/essence/team/CommandSetthome.java
+++ src/main/java/net/lewmc/essence/team/CommandSetthome.java
@@ -6,19 +6,15 @@ import net.lewmc.essence.core.UtilMessage;
 import net.lewmc.essence.core.UtilPermission;
 import net.lewmc.essence.teleportation.home.UtilHome;
 import net.lewmc.foundry.Files;
-import net.lewmc.foundry.Logger;
 import net.lewmc.foundry.Security;
+import net.lewmc.foundry.command.FoundryPlayerCommand;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
-public class CommandSetthome implements CommandExecutor {
+public class CommandSetthome extends FoundryPlayerCommand {
     private final Essence plugin;
-    private final Logger log;
-
     /**
      * Constructor for the SetthomeCommand class.
      *
@@ -26,97 +22,93 @@ public class CommandSetthome implements CommandExecutor {
      */
     public CommandSetthome(Essence plugin) {
         this.plugin = plugin;
-        this.log = new Logger(plugin.config);
     }
 
     /**
-     * @param cs            Information about who sent the command - player or console.
-     * @param command       Information about what command was sent.
-     * @param s             Command label - not used here.
-     * @param args          The command's arguments.
-     * @return boolean true/false - was the command accepted and processed or not?
+     * The required permission.
+     *
+     * @return String - The permission string
      */
     @Override
-    public boolean onCommand(
-        @NotNull CommandSender cs,
-        @NotNull Command command,
-        @NotNull String s,
-        String[] args
-    ) {
-        if (command.getName().equalsIgnoreCase("setthome")) {
-            UtilCommand cmd = new UtilCommand(this.plugin, cs);
-            if (cmd.isDisabled("setthome")) { return cmd.disabled(); }
+    protected String requiredPermission() {
+        return "essence.home.team.create";
+    }
 
-            if (!(cs instanceof Player p)) { return this.log.noConsole(); }
+    /**
+     * @param cs      Information about who sent the command - player or console.
+     * @param command Information about what command was sent.
+     * @param s       Command label - not used here.
+     * @param args    The command's arguments.
+     * @return boolean  true/false - was the command accepted and processed or not?
+     */
+    @Override
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        UtilCommand cmd = new UtilCommand(this.plugin, cs);
+        if (cmd.isDisabled("setthome")) {
+            return cmd.disabled();
+        }
 
-            UtilMessage message = new UtilMessage(this.plugin, cs);
-            UtilPermission permission = new UtilPermission(this.plugin, cs);
+        Player p = (Player) cs;
 
-            UtilTeam tu = new UtilTeam(this.plugin, message);
-            String team = tu.getPlayerTeam(p.getUniqueId());
+        UtilMessage msg = new UtilMessage(this.plugin, cs);
 
-            if (team == null) {
-                message.send("team", "noteam");
-                return true;
-            }
+        UtilTeam tu = new UtilTeam(this.plugin, msg);
+        String team = tu.getPlayerTeam(p.getUniqueId());
 
-            if (!tu.getRule(team, "allow-team-homes")) {
-                message.send("team", "disallowedhomes");
-                return true;
-            }
-
-            if (permission.has("essence.home.team.create")) {
-
-                String name;
-                if (args.length == 0) {
-                    name = "home";
-                } else {
-                    name = args[0];
-                }
-
-                Location loc = p.getLocation();
-                Files dataUtil = new Files(this.plugin.config, this.plugin);
-                dataUtil.load("data/teams/"+team+".yml");
-
-                if (new Security(this.plugin.config).hasSpecialCharacters(name.toLowerCase())) {
-                    dataUtil.close();
-                    message.send("teamhome", "specialchars");
-                    return true;
-                }
-
-                String homeName = "homes." + name.toLowerCase();
-
-                if (dataUtil.get(homeName) != null) {
-                    dataUtil.close();
-                    message.send("teamhome", "alreadyexists");
-                    return true;
-                }
-
-                UtilHome hu = new UtilHome(this.plugin);
-                int homeLimit = permission.getTeamHomesLimit(p);
-                if (hu.getTeamHomeCount(p) >= homeLimit && homeLimit != -1) {
-                    message.send("teamhome", "hitlimit");
-                    return true;
-                }
-
-                dataUtil.set(homeName + ".creator", p.getUniqueId().toString());
-                dataUtil.set(homeName + ".world", loc.getWorld().getName());
-                dataUtil.set(homeName + ".X", loc.getX());
-                dataUtil.set(homeName + ".Y", loc.getY());
-                dataUtil.set(homeName + ".Z", loc.getZ());
-                dataUtil.set(homeName + ".yaw", loc.getYaw());
-                dataUtil.set(homeName + ".pitch", loc.getPitch());
-
-                // Save the configuration to the file
-                dataUtil.save();
-
-                message.send("teamhome", "created", new String[] { name });
-            } else {
-                permission.not();
-            }
+        if (team == null) {
+            msg.send("team", "noteam");
             return true;
         }
 
-        return false;
+        if (!tu.getRule(team, "allow-team-homes")) {
+            msg.send("team", "disallowedhomes");
+            return true;
+        }
+        String name;
+        if (args.length == 0) {
+            name = "home";
+        } else {
+            name = args[0];
+        }
+
+        Location loc = p.getLocation();
+        Files dataUtil = new Files(this.plugin.config, this.plugin);
+        dataUtil.load("data/teams/" + team + ".yml");
+
+        if (new Security(this.plugin.config).hasSpecialCharacters(name.toLowerCase())) {
+            dataUtil.close();
+            msg.send("teamhome", "specialchars");
+            return true;
+        }
+
+        String homeName = "homes." + name.toLowerCase();
+
+        if (dataUtil.get(homeName) != null) {
+            dataUtil.close();
+            msg.send("teamhome", "alreadyexists");
+            return true;
+        }
+
+        UtilHome hu = new UtilHome(this.plugin);
+        int homeLimit = new UtilPermission(this.plugin, cs).getTeamHomesLimit(p);
+        if (hu.getTeamHomeCount(p) >= homeLimit && homeLimit != -1) {
+            msg.send("teamhome", "hitlimit");
+            return true;
+        }
+
+        dataUtil.set(homeName + ".creator", p.getUniqueId().toString());
+        dataUtil.set(homeName + ".world", loc.getWorld().getName());
+        dataUtil.set(homeName + ".X", loc.getX());
+        dataUtil.set(homeName + ".Y", loc.getY());
+        dataUtil.set(homeName + ".Z", loc.getZ());
+        dataUtil.set(homeName + ".yaw", loc.getYaw());
+        dataUtil.set(homeName + ".pitch", loc.getPitch());
+
+        // Save the configuration to the file
+        dataUtil.save();
+
+        msg.send("teamhome", "created", new String[]{name});
+
+        return true;
     }
 }
--- src/main/java/net/lewmc/essence/team/CommandThomas.java
+++ src/main/java/net/lewmc/essence/team/CommandThomas.java
@@ -3,18 +3,14 @@ package net.lewmc.essence.team;
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.core.UtilCommand;
 import net.lewmc.essence.core.UtilMessage;
-import net.lewmc.essence.core.UtilPermission;
 import net.lewmc.essence.teleportation.home.UtilHome;
-import net.lewmc.foundry.Logger;
+import net.lewmc.foundry.command.FoundryPlayerCommand;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
-public class CommandThomas implements CommandExecutor {
+public class CommandThomas extends FoundryPlayerCommand {
     private final Essence plugin;
-    private final Logger log;
 
     /**
      * Constructor for the ThomesCommand class.
@@ -22,61 +18,55 @@ public class CommandThomas implements CommandExecutor {
      */
     public CommandThomas(Essence plugin) {
         this.plugin = plugin;
-        this.log = new Logger(plugin.config);
     }
 
     /**
-     * @param cs Information about who sent the command - player or console.
-     * @param command Information about what command was sent.
-     * @param s Command label - not used here.
-     * @param args The command's arguments.
-     * @return boolean true/false - was the command accepted and processed or not?
+     * The required permission.
+     * @return String - The permission string.
      */
     @Override
-    public boolean onCommand(
-        @NotNull CommandSender cs,
-        @NotNull Command command,
-        @NotNull String s,
-        String[] args
-    ) {
-        if (command.getName().equalsIgnoreCase("thomes")) {
-            UtilCommand cmd = new UtilCommand(this.plugin, cs);
-            if (cmd.isDisabled("thomes")) { return cmd.disabled(); }
+    protected String requiredPermission() {
+        return "essence.home.team.list";
+    }
 
-            if (!(cs instanceof Player p)) { return this.log.noConsole(); }
+    /**
+     * @param cs        Information about who sent the command - player or console.
+     * @param command   Information about what command was sent.
+     * @param s         Command label - not used here.
+     * @param args      The command's arguments.
+     * @return boolean  true/false - was the command accepted and processed or not?
+     */
+    @Override
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        UtilCommand cmd = new UtilCommand(this.plugin, cs);
+        if (cmd.isDisabled("thomes")) {return cmd.disabled();}
 
-            UtilMessage msg = new UtilMessage(this.plugin, cs);
-            UtilPermission perms = new UtilPermission(this.plugin, cs);
+        Player p = (Player) cs;
 
-            UtilTeam tu = new UtilTeam(this.plugin, msg);
-            String team = tu.getPlayerTeam(p.getUniqueId());
+        UtilMessage msg = new UtilMessage(this.plugin, cs);
 
-            if (team == null) {
-                msg.send("team", "noteam");
-                return true;
-            }
+        UtilTeam tu = new UtilTeam(this.plugin, msg);
+        String team = tu.getPlayerTeam(p.getUniqueId());
 
-            if (!tu.getRule(team, "allow-team-homes")) {
-                msg.send("team", "disallowedhomes");
-                return true;
-            }
-
-            if (perms.has("essence.home.team.list")) {
-                UtilHome hu = new UtilHome(this.plugin);
-                StringBuilder setHomes = hu.getTeamHomesList(team);
-
-                if (setHomes == null) {
-                    msg.send("teamhome", "noneset");
-                    return true;
-                }
-
-                msg.send("teamhome", "list", new String[] { setHomes.toString() });
-            } else {
-                perms.not();
-            }
+        if (team == null) {
+            msg.send("team", "noteam");
             return true;
         }
 
-        return false;
+        if (!tu.getRule(team, "allow-team-homes")) {
+            msg.send("team", "disallowedhomes");
+            return true;
+        }
+
+        UtilHome hu = new UtilHome(this.plugin);
+        StringBuilder setHomes = hu.getTeamHomesList(team);
+
+        if (setHomes == null) {
+            msg.send("teamhome", "noneset");
+            return true;
+        }
+
+        msg.send("teamhome", "list", new String[]{setHomes.toString()});
+        return true;
     }
 }
