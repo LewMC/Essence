8a32b830723ad7cbed8c2babcb9ab8e56b8d3f04
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-20 12:47:10 +0100

    Migrate to Foundry.

--- src/main/java/net/lewmc/essence/teleportation/CommandBack.java
+++ src/main/java/net/lewmc/essence/teleportation/CommandBack.java
@@ -4,20 +4,17 @@ import net.lewmc.essence.Essence;
 import net.lewmc.essence.core.UtilCommand;
 import net.lewmc.essence.core.UtilLocation;
 import net.lewmc.essence.core.UtilMessage;
-import net.lewmc.essence.core.UtilPermission;
 import net.lewmc.essence.teleportation.tp.UtilTeleport;
 import net.lewmc.foundry.Files;
-import net.lewmc.foundry.Logger;
+import net.lewmc.foundry.command.FoundryPlayerCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class CommandBack implements CommandExecutor {
+public class CommandBack extends FoundryPlayerCommand {
     private final Essence plugin;
 
     /**
@@ -30,62 +27,52 @@ public class CommandBack implements CommandExecutor {
     }
 
     /**
-     * @param cs            Information about who sent the command - player or console.
-     * @param command       Information about what command was sent.
-     * @param s             Command label - not used here.
-     * @param args          The command's arguments.
-     * @return boolean true/false - was the command accepted and processed or not?
+     * The required permission.
+     *
+     * @return String - The permission string.
      */
     @Override
-    public boolean onCommand(
-            @NotNull CommandSender cs,
-            @NotNull Command command,
-            @NotNull String s,
-            String[] args
-    ) {
-        if (command.getName().equalsIgnoreCase("back")) {
-            UtilCommand cmd = new UtilCommand(this.plugin, cs);
-            if (cmd.isDisabled("back")) { return cmd.disabled(); }
+    protected String requiredPermission() {
+        return "essence.teleport.back";
+    }
 
-            if (!(cs instanceof Player p)) { return new Logger(this.plugin.config).noConsole(); }
+    /**
+     * @param cs        Information about who sent the command - player or console.
+     * @param command   Information about what command was sent.
+     * @param s         Command label - not used here.
+     * @param args      The command's arguments.
+     * @return boolean  true/false - was the command accepted and processed or not?
+     */
+    @Override
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        UtilCommand cmd = new UtilCommand(this.plugin, cs);
+        if (cmd.isDisabled("back")) { return cmd.disabled(); }
 
-            UtilPermission perms = new UtilPermission(this.plugin, cs);
+        Player p =  (Player) cs;
 
-            if (perms.has("essence.teleport.back")) {
-                UtilMessage msg = new UtilMessage(this.plugin, cs);
-                Files playerData = new Files(this.plugin.config, this.plugin);
+        Files playerData = new Files(this.plugin.config, this.plugin);
+        playerData.load(playerData.playerDataFile(p));
 
-                playerData.load(playerData.playerDataFile(p));
-
-                if (playerData.get("last-location") == null) {
-                    msg.send("back", "cant");
-                    return true;
-                }
-
-                UtilLocation locationUtil = new UtilLocation(this.plugin);
-                locationUtil.UpdateLastLocation(p);
-
-                UtilTeleport tp = new UtilTeleport(plugin);
-                tp.doTeleport(
-                        p,
-                        Bukkit.getServer().getWorld(Objects.requireNonNull(playerData.getString("last-location.world"))),
-                        playerData.getDouble("last-location.X"),
-                        playerData.getDouble("last-location.Y"),
-                        playerData.getDouble("last-location.Z"),
-                        (float) playerData.getDouble("last-location.yaw"),
-                        (float) playerData.getDouble("last-location.pitch"),
-                        0
-                );
-
-                playerData.close();
-
-                msg.send("back", "going");
-
-            } else {
-                return perms.not();
-            }
+        if (playerData.get("last-location") == null) {
+            new UtilMessage(this.plugin, cs).send("back", "cant");
             return true;
         }
-        return false;
+
+        new UtilLocation(this.plugin).UpdateLastLocation(p);
+        new UtilTeleport(this.plugin).doTeleport(
+                p,
+                Bukkit.getServer().getWorld(Objects.requireNonNull(playerData.getString("last-location.world"))),
+                playerData.getDouble("last-location.X"),
+                playerData.getDouble("last-location.Y"),
+                playerData.getDouble("last-location.Z"),
+                (float) playerData.getDouble("last-location.yaw"),
+                (float) playerData.getDouble("last-location.pitch"),
+                0
+        );
+
+        playerData.close();
+
+        new UtilMessage(this.plugin, cs).send("back", "going");
+        return true;
     }
 }
