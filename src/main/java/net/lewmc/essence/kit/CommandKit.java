7367a48624823f5a90b89f27c333f9b8c417ab28
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-20 08:29:26 +0100

    Migrate to Foundry.

--- src/main/java/net/lewmc/essence/kit/CommandKit.java
+++ src/main/java/net/lewmc/essence/kit/CommandKit.java
@@ -5,19 +5,17 @@ import net.lewmc.essence.core.UtilCommand;
 import net.lewmc.essence.core.UtilMessage;
 import net.lewmc.essence.core.UtilPermission;
 import net.lewmc.foundry.Files;
-import net.lewmc.foundry.Logger;
+import net.lewmc.foundry.command.FoundryPlayerCommand;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
 /**
  * /kit command.
  */
-public class CommandKit implements CommandExecutor {
+public class CommandKit extends FoundryPlayerCommand {
 
     private final Essence plugin;
 
@@ -30,73 +28,72 @@ public class CommandKit implements CommandExecutor {
     }
 
     /**
-     * @param cs Information about who sent the command - player or console.
-     * @param command Information about what command was sent.
-     * @param s Command label - not used here.
-     * @param args The command's arguments.
+     * The required permission
+     * @return String - The permission string.
+     */
+    @Override
+    protected String requiredPermission() {
+        return null;
+    }
+
+    /**
+     * @param cs        Information about who sent the command - player or console.
+     * @param command   Information about what command was sent.
+     * @param s         Command label - not used here.
+     * @param args      The command's arguments.
      * @return boolean true/false - was the command accepted and processed or not?
      */
     @Override
-    public boolean onCommand(
-            @NotNull CommandSender cs,
-            @NotNull Command command,
-            @NotNull String s,
-            @NotNull String[] args)
-    {
-        if (command.getName().equalsIgnoreCase("kit")) {
-            UtilCommand cmd = new UtilCommand(this.plugin, cs);
-            if (cmd.isDisabled("kit")) { return cmd.disabled(); }
-            if (cmd.console(cs)) { return new Logger(this.plugin.config).noConsole(); }
+    protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
+        UtilCommand cmd = new UtilCommand(this.plugin, cs);
+        if (cmd.isDisabled("kit")) { return cmd.disabled(); }
 
-            UtilMessage msg = new UtilMessage(this.plugin, cs);
-            Player p = (Player) cs;
-            UtilPermission perms = new UtilPermission(this.plugin, cs);
+        Player p = (Player) cs;
 
-            if (args.length == 0) {
-                StringBuilder kits = new StringBuilder("No kits found.");
+        if (args.length == 0) {
+            StringBuilder kits = new StringBuilder("No kits found.");
 
-                Files kitData = new Files(this.plugin.config, this.plugin);
-                kitData.load("data/kits.yml");
-                Set<String> keys = kitData.getKeys("kits", false);
+            Files kitData = new Files(this.plugin.config, this.plugin);
+            kitData.load("data/kits.yml");
+            Set<String> keys = kitData.getKeys("kits", false);
 
-                int i = 0;
-                for (Object object : keys) {
-                    if (kitData.get("kits." + object + ".permission") == null) {
+            int i = 0;
+            for (Object object : keys) {
+                if (kitData.get("kits." + object + ".permission") == null) {
+                    if (i == 0) {
+                        kits = new StringBuilder(object.toString());
+                    } else {
+                        kits.append(", ").append(object.toString());
+                    }
+                } else {
+                    UtilPermission perms = new UtilPermission(this.plugin, cs);
+                    if (perms.has(kitData.get("kits." + object + ".permission").toString()) || perms.has("essence.kits.all")) {
                         if (i == 0) {
                             kits = new StringBuilder(object.toString());
                         } else {
                             kits.append(", ").append(object.toString());
                         }
-                    } else {
-                        if (perms.has(kitData.get("kits." + object + ".permission").toString()) || perms.has("essence.kits.all")) {
-                            if (i == 0) {
-                                kits = new StringBuilder(object.toString());
-                            } else {
-                                kits.append(", ").append(object.toString());
-                            }
-                        }
                     }
-                    i++;
                 }
-
-                kitData.close();
-
-                msg.send("kit", "select", new String[] { kits.toString() });
-            } else {
-                UtilKit kit = new UtilKit(this.plugin, p);
-
-                if (kit.giveKit(args[0]) == 0) {
-                    msg.send("kit", "done", new String[] { args[0] });
-                } else if (kit.giveKit(args[0]) == 1) {
-                    msg.send("kit", "nopermission");
-                } else if (kit.giveKit(args[0]) == 2) {
-                    msg.send("kit", "notexist");
-                } else if (kit.giveKit(args[0]) == 3) {
-                    msg.send("kit", "max");
-                }
+                i++;
             }
-            return true;
+
+            kitData.close();
+
+            new UtilMessage(this.plugin, cs).send("kit", "select", new String[] { kits.toString() });
+        } else {
+            UtilKit kit = new UtilKit(this.plugin, p);
+            UtilMessage msg = new UtilMessage(this.plugin, cs);
+            if (kit.giveKit(args[0]) == 0) {
+                msg.send("kit", "done", new String[] { args[0] });
+            } else if (kit.giveKit(args[0]) == 1) {
+                msg.send("kit", "nopermission");
+            } else if (kit.giveKit(args[0]) == 2) {
+                msg.send("kit", "notexist");
+            } else if (kit.giveKit(args[0]) == 3) {
+                msg.send("kit", "max");
+            }
         }
-        return false;
+        return true;
     }
 }
