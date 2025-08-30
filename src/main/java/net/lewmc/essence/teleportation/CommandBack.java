e41093bc85c0169dd1dd1eefe7136fd6fa1e0cb2
YXC0915 <y2692290472@gmail.com>
2025-08-30 23:31:50 +0800

    feat(teleport): 为/back命令添加传送其他玩家功能
    -扩展/back命令功能，允许管理员传送其他玩家回到他们上次的位置
    
    -移除昵称命令的特殊字符限制以支持中文昵称
    -修复nick指令注册的别名显示异常的问题

--- src/main/java/net/lewmc/essence/teleportation/CommandBack.java
+++ src/main/java/net/lewmc/essence/teleportation/CommandBack.java
@@ -2,9 +2,10 @@ package net.lewmc.essence.teleportation;
 
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.core.UtilMessage;
+import net.lewmc.essence.core.UtilPermission;
 import net.lewmc.essence.teleportation.tp.UtilTeleport;
 import net.lewmc.foundry.Files;
-import net.lewmc.foundry.command.FoundryPlayerCommand;
+import net.lewmc.foundry.command.FoundryCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,7 +16,7 @@ import java.util.Objects;
 /**
  * /back command.
  */
-public class CommandBack extends FoundryPlayerCommand {
+public class CommandBack extends FoundryCommand {
     private final Essence plugin;
 
     /**
@@ -46,13 +47,41 @@ public class CommandBack extends FoundryPlayerCommand {
      */
     @Override
     protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
-        Player p =  (Player) cs;
+        UtilMessage msg = new UtilMessage(this.plugin, cs);
 
+        if (args.length == 0) {
+            // /back - 传送自己回到上一个位置
+            if (!(cs instanceof Player)) {
+                msg.send("back", "usage");
+                return true;
+            }
+            return this.backSelf((Player) cs, msg);
+        } else if (args.length == 1) {
+            // /back <player> - 传送其他玩家回到上一个位置
+            UtilPermission perms = new UtilPermission(this.plugin, cs);
+            if (!perms.has("essence.teleport.back.other")) {
+                return perms.not();
+            }
+            return this.backOther(cs, msg, args[0]);
+        } else {
+            msg.send("back", "usage");
+            return true;
+        }
+    }
+
+    /**
+     * 传送玩家自己回到上一个位置
+     * @param p 玩家
+     * @param msg 消息工具
+     * @return 是否成功
+     */
+    private boolean backSelf(Player p, UtilMessage msg) {
         Files playerData = new Files(this.plugin.foundryConfig, this.plugin);
         playerData.load(playerData.playerDataFile(p));
 
         if (playerData.get("last-location") == null) {
-            new UtilMessage(this.plugin, cs).send("back", "cant");
+            msg.send("back", "cant");
+            playerData.close();
             return true;
         }
 
@@ -68,8 +97,51 @@ public class CommandBack extends FoundryPlayerCommand {
         );
 
         playerData.close();
+        msg.send("back", "going");
+        return true;
+    }
 
-        new UtilMessage(this.plugin, cs).send("back", "going");
+    /**
+     * 传送其他玩家回到上一个位置
+     * @param cs 命令发送者
+     * @param msg 消息工具
+     * @param targetName 目标玩家名称
+     * @return 是否成功
+     */
+    private boolean backOther(CommandSender cs, UtilMessage msg, String targetName) {
+        Player targetPlayer = Bukkit.getPlayer(targetName);
+        
+        if (targetPlayer == null) {
+            msg.send("generic", "playernotfound");
+            return true;
+        }
+
+        Files playerData = new Files(this.plugin.foundryConfig, this.plugin);
+        playerData.load(playerData.playerDataFile(targetPlayer));
+
+        if (playerData.get("last-location") == null) {
+            msg.send("back", "cantother", new String[]{targetPlayer.getName()});
+            playerData.close();
+            return true;
+        }
+
+        new UtilTeleport(this.plugin).doTeleport(
+                targetPlayer,
+                Bukkit.getServer().getWorld(Objects.requireNonNull(playerData.getString("last-location.world"))),
+                playerData.getDouble("last-location.X"),
+                playerData.getDouble("last-location.Y"),
+                playerData.getDouble("last-location.Z"),
+                (float) playerData.getDouble("last-location.yaw"),
+                (float) playerData.getDouble("last-location.pitch"),
+                0
+        );
+
+        playerData.close();
+        
+        // 通知命令发送者和目标玩家
+        msg.send("back", "goingother", new String[]{targetPlayer.getName()});
+        new UtilMessage(this.plugin, targetPlayer).send("back", "sentby", new String[]{cs.getName()});
+        
         return true;
     }
 }
