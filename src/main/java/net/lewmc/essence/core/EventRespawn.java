21ba8d7cd6e43879b470f80881644b21a49287d6
Lewis Milburn <lewiscmilburn@gmail.com>
2025-11-18 23:08:18 +0000

    Update respawn system.

--- src/main/java/net/lewmc/essence/core/EventRespawn.java
+++ src/main/java/net/lewmc/essence/core/EventRespawn.java
@@ -1,10 +1,8 @@
 package net.lewmc.essence.core;
 
 import net.lewmc.essence.Essence;
-import net.lewmc.essence.teleportation.tp.UtilTeleport;
-import net.lewmc.foundry.Files;
-import net.lewmc.foundry.Logger;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -32,85 +30,40 @@ public class EventRespawn implements Listener {
      */
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
-        Logger log = new Logger(this.plugin.foundryConfig);
-        UtilMessage message = new UtilMessage(this.plugin, event.getPlayer());
 
-        Files config = new Files(this.plugin.foundryConfig, this.plugin);
-        config.load("config.yml");
-        String spawnName = config.getString("teleportation.spawn.main-spawn-world");
-        boolean alwaysSpawn = config.getBoolean("teleportation.spawn.always-spawn");
-        config.close();
+        UUID uuid = event.getPlayer().getUniqueId();
 
-        UUID epID = event.getPlayer().getUniqueId();
+        UtilPlayer up = new UtilPlayer(plugin);
+        boolean alwaysSpawn = plugin.getConfig().getBoolean("teleportation.spawn.always-spawn");
 
-        UtilPlayer up = new UtilPlayer(this.plugin);
-        if ((up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_WORLD) != null) && !alwaysSpawn) {
-            UtilTeleport tp = new UtilTeleport(plugin);
-            World world = Bukkit.getServer().getWorld((String)up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_WORLD));
+        if (!alwaysSpawn && up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_WORLD) != null) {
+            String worldName = up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_WORLD).toString();
+            World world = Bukkit.getWorld(worldName);
+
             if (world != null) {
-                tp.doTeleport(
-                        event.getPlayer(),
-                        Bukkit.getServer().getWorld(up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_WORLD).toString()),
-                        (Double) up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_X),
-                        (Double) up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_Y),
-                        (Double) up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_Z),
-                        (Float) up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_YAW),
-                        (Float) up.getPlayer(epID, UtilPlayer.KEYS.LAST_SLEEP_PITCH),
-                        0,
-                        false
-                );
-
+                event.setRespawnLocation(new Location(
+                        world,
+                        (double) up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_X),
+                        (double) up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_Y),
+                        (double) up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_Z),
+                        ((Float) up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_YAW)),
+                        ((Float) up.getPlayer(uuid, UtilPlayer.KEYS.LAST_SLEEP_PITCH))
+                ));
                 return;
             }
         }
 
-        Files spawns = new Files(this.plugin.foundryConfig, this.plugin);
-        spawns.load("data/spawns.yml");
-
-        if (spawns.get("spawn."+spawnName) == null) {
-            if (
-                Bukkit.getServer().getWorld(spawnName).getSpawnLocation() != null &&
-                Bukkit.getServer().getWorld(spawnName).getSpawnLocation().getY() >= 10
-            ) {
-                UtilTeleport tp = new UtilTeleport(plugin);
-                tp.doTeleport(
-                        event.getPlayer(),
-                        Bukkit.getServer().getWorld(spawnName),
-                        Bukkit.getServer().getWorld(spawnName).getSpawnLocation().getX(),
-                        Bukkit.getServer().getWorld(spawnName).getSpawnLocation().getY(),
-                        Bukkit.getServer().getWorld(spawnName).getSpawnLocation().getZ(),
-                        Bukkit.getServer().getWorld(spawnName).getSpawnLocation().getYaw(),
-                        Bukkit.getServer().getWorld(spawnName).getSpawnLocation().getPitch(),
-                        0,
-                        false
-                );
-            } else {
-                message.send("spawn", "notexist");
-                log.info("Failed to respawn player - world '"+Bukkit.getServer().getWorld(spawnName)+"' does not exist.");
-            }
-        } else {
-            UtilTeleport tp = new UtilTeleport(plugin);
-
-            World world = Bukkit.getServer().getWorld(spawnName);
-            
-            if (world == null) {
-                message.send("spawn", "notexist");
-                this.plugin.log.severe("World "+spawnName+" not found.");
-            } else {
-                tp.doTeleport(
-                        event.getPlayer(),
-                        world,
-                        spawns.getDouble("spawn." + spawnName + ".X"),
-                        spawns.getDouble("spawn." + spawnName + ".Y"),
-                        spawns.getDouble("spawn." + spawnName + ".Z"),
-                        (float) spawns.getDouble("spawn." + spawnName + ".yaw"),
-                        (float) spawns.getDouble("spawn." + spawnName + ".pitch"),
-                        0,
-                        false
-                );
-            }
+        String spawnName = plugin.getConfig().getString("teleportation.spawn.main-spawn-world");
+        if (spawnName == null) {
+            plugin.getLogger().warning("Spawn world " + spawnName + " not found!");
+            return;
         }
+        World world = Bukkit.getWorld(spawnName);
 
-        spawns.close();
+        if (world != null) {
+            event.setRespawnLocation(world.getSpawnLocation());
+        } else {
+            plugin.getLogger().warning("Spawn world " + spawnName + " not found!");
+        }
     }
 }
