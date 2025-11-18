524674ea50dc447f04ab391b7d0204a0efae7729
Lewis Milburn <lewiscmilburn@gmail.com>
2025-11-18 12:48:39 +0000

    /bal - NPDC

--- src/main/java/net/lewmc/essence/economy/CommandBalance.java
+++ src/main/java/net/lewmc/essence/economy/CommandBalance.java
@@ -3,6 +3,7 @@ package net.lewmc.essence.economy;
 import net.lewmc.essence.Essence;
 import net.lewmc.essence.core.TypePlayer;
 import net.lewmc.essence.core.UtilMessage;
+import net.lewmc.essence.core.UtilPlayer;
 import net.lewmc.foundry.command.FoundryCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -42,12 +43,7 @@ public class CommandBalance extends FoundryCommand {
     @Override
     protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
         if (cs instanceof Player p) {
-            TypePlayer playerData = this.plugin.players.get(p.getUniqueId());
-            if (playerData != null) {
-                new UtilMessage(this.plugin, cs).send("economy", "balance", new String[]{this.plugin.config.get("economy.symbol").toString() + playerData.economy.balance});
-            } else {
-                new UtilMessage(this.plugin, cs).send("economy", "balance", new String[]{this.plugin.config.get("economy.symbol").toString() + "0.0"});
-            }
+            new UtilMessage(this.plugin, cs).send("economy", "balance", new String[]{this.plugin.config.get("economy.symbol").toString() + new UtilPlayer(this.plugin).getPlayer(p, UtilPlayer.PLAYER_KEYS.ECONOMY_BALANCE)});
         } else {
             new UtilMessage(this.plugin, cs).send("economy", "balance", new String[]{this.plugin.config.get("economy.symbol").toString() + "Infinity"});
         }
