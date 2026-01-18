01ecd9450e4e4870de694939d68259959c9daf35
Lewis Milburn <lewiscmilburn@gmail.com>
2026-01-18 11:42:58 +0000

    Set fly and walk speed independently of one-another #311

--- src/main/java/net/lewmc/essence/stats/CommandSpeed.java
+++ src/main/java/net/lewmc/essence/stats/CommandSpeed.java
@@ -43,7 +43,7 @@ public class CommandSpeed extends FoundryCommand {
     protected boolean onRun(CommandSender cs, Command command, String s, String[] args) {
         UtilMessage message = new UtilMessage(this.plugin, cs);
 
-        if (args.length == 1) {
+        if (args.length == 1 || (args.length == 2 && (args[1].equalsIgnoreCase("fly") || args[1].equalsIgnoreCase("walk")))) {
             if (cs instanceof Player p) {
                 return this.speedSelf(p, message, args);
             } else {
@@ -51,7 +51,7 @@ public class CommandSpeed extends FoundryCommand {
                 return true;
             }
         }
-        else if (args.length == 2) {
+        else if (args.length == 2 || (args.length == 3 && (args[2].equalsIgnoreCase("fly") || args[2].equalsIgnoreCase("walk")))) {
             return this.speedOther(new UtilPermission(this.plugin, cs), cs, message, args);
         } else {
             if (cs instanceof Player p) {
@@ -72,9 +72,17 @@ public class CommandSpeed extends FoundryCommand {
     private boolean speedSelf(Player p, UtilMessage msg, String[] args) {
         try {
             if (Objects.equals(args[0], "off") || Objects.equals(args[0], "reset") || Objects.equals(args[0], "default")) {
-                p.setWalkSpeed(0.2F);
-                p.setFlySpeed(0.1F);
-                msg.send("speed", "reset");
+                if (args.length == 2 && args[1].equalsIgnoreCase("fly")) {
+                    p.setFlySpeed(0.1F);
+                    msg.send("speed", "resetfly");
+                } else if (args.length == 2 && args[1].equalsIgnoreCase("walk")) {
+                    p.setWalkSpeed(0.2F);
+                    msg.send("speed", "resetwalk");
+                } else {
+                    p.setWalkSpeed(0.2F);
+                    p.setFlySpeed(0.1F);
+                    msg.send("speed", "reset");
+                }
             } else {
                 float speed = Float.parseFloat(args[0]);
 
@@ -83,9 +91,17 @@ public class CommandSpeed extends FoundryCommand {
                     return true;
                 }
 
-                p.setWalkSpeed(speed/10);
-                p.setFlySpeed(speed/10);
-                msg.send("speed", "set", new String[]{ args[0] });
+                if (args.length == 2 && args[1].equalsIgnoreCase("fly")) {
+                    p.setFlySpeed(speed/10);
+                    msg.send("speed", "setfly", new String[]{ args[0] });
+                } else if (args.length == 2 && args[1].equalsIgnoreCase("walk")) {
+                    p.setWalkSpeed(speed/10);
+                    msg.send("speed", "setwalk", new String[]{ args[0] });
+                } else {
+                    p.setWalkSpeed(speed/10);
+                    p.setFlySpeed(speed/10);
+                    msg.send("speed", "set", new String[]{ args[0] });
+                }
             }
         } catch (NumberFormatException | NullPointerException e) {
             msg.send("speed", "usage");
@@ -110,10 +126,20 @@ public class CommandSpeed extends FoundryCommand {
             if (p != null) {
                 try {
                     if (Objects.equals(args[0], "off") || Objects.equals(args[0], "reset") || Objects.equals(args[0], "default")) {
-                        p.setWalkSpeed(0.2F);
-                        p.setFlySpeed(0.1F);
-                        msg.send("speed", "resetbyother", new String[]{ cs.getName() });
-                        msg.sendTo(cs, "speed", "resetother", new String[]{ p.getName() });
+                        if (args.length == 3 && args[2].equalsIgnoreCase("fly")) {
+                            p.setFlySpeed(0.1F);
+                            msg.send("speed", "resetflybyother", new String[]{ cs.getName() });
+                            msg.sendTo(cs, "speed", "resetflyother", new String[]{ p.getName() });
+                        } else if (args.length == 3 && args[2].equalsIgnoreCase("walk")) {
+                            p.setWalkSpeed(0.2F);
+                            msg.send("speed", "resetwalkbyother", new String[]{ cs.getName() });
+                            msg.sendTo(cs, "speed", "resetwalkother", new String[]{ p.getName() });
+                        } else {
+                            p.setWalkSpeed(0.2F);
+                            p.setFlySpeed(0.1F);
+                            msg.send("speed", "resetbyother", new String[]{ cs.getName() });
+                            msg.sendTo(cs, "speed", "resetother", new String[]{ p.getName() });
+                        }
                     } else {
                         float speed = Float.parseFloat(args[0]);
 
@@ -122,10 +148,20 @@ public class CommandSpeed extends FoundryCommand {
                             return true;
                         }
 
-                        p.setWalkSpeed(speed/10);
-                        p.setFlySpeed(speed/10);
-                        msg.send("speed", "setbyother", new String[]{cs.getName(), args[0]});
-                        msg.sendTo(cs, "speed", "setother", new String[]{args[1],args[0]});
+                        if (args.length == 3 && args[2].equalsIgnoreCase("fly")) {
+                            p.setFlySpeed(speed/10);
+                            msg.send("speed", "setflybyother", new String[]{cs.getName(), args[0]});
+                            msg.sendTo(cs, "speed", "setflyother", new String[]{args[1],args[0]});
+                        } else if (args.length == 3 && args[2].equalsIgnoreCase("walk")) {
+                            p.setWalkSpeed(speed/10);
+                            msg.send("speed", "setwalkbyother", new String[]{cs.getName(), args[0]});
+                            msg.sendTo(cs, "speed", "setwalkother", new String[]{args[1],args[0]});
+                        } else {
+                            p.setWalkSpeed(speed/10);
+                            p.setFlySpeed(speed/10);
+                            msg.send("speed", "setbyother", new String[]{cs.getName(), args[0]});
+                            msg.sendTo(cs, "speed", "setother", new String[]{args[1],args[0]});
+                        }
                     }
                 } catch (NumberFormatException | NullPointerException e) {
                     msg.send("speed", "usage");
